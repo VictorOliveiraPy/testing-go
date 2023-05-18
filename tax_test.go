2de9201e2eaef0ba154ba136db4e1ce901d1023c
Victor Hugo <victorblog410@gmail.com>
2023-05-18 15:17:04 -0300

    adding testing automated in batch

--- tax_test.go
+++ tax_test.go
@@ -14,3 +14,31 @@ func TestCalculateTax(t *testing.T) {
 		}
 
 	}
+
+
+func TestCalculateTaxBatch(t *testing.T) {
+	type calcTax struct {
+		amount, expect float64
+	}
+
+	table := []calcTax{
+		{500.0, 5.0},
+		{800.0, 5.0},
+		{1000.0, 10.0},
+		{1500.0, 10.0},
+		{2000.0, 10.0},
+		{2500.0, 10.0},
+        {3000.0, 10.0},
+        {3500.0, 10.0},
+        {4000.0, 10.0},
+        {4500.0, 10.0},
+
+	}
+	for _, item := range table {
+		result := CalculateTax(item.amount)
+        if result != item.expect {
+            t.Errorf("Expected %f, but got %f", item.expect, result)
+        }
+    }
+
+	}
